af746622c69532b46b5781b04773b22bafb5167e
Anufriyeva <126349829+Anufriyeva@users.noreply.github.com>
2023-12-03 12:56:23 +0200

    hw02-express

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,19 @@
-const express = require('express')
+const express = require("express")
 
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const ctrl = require("../../controllers/contacts");
+const { validateBody } = require("../../middlewares");
+const schemas = require("../../schemas/contacts");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/', ctrl.listContacts) 
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/:id', ctrl.getContactById)
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post('/', validateBody(schemas.addSchema), ctrl.addContact)
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete('/:id', ctrl.removeContact)
 
-module.exports = router
+router.put('/:id', ctrl.updateContactById)
+
+module.exports = router;
---
+++ schemas/contacts.js
@@ -0,0 +1,11 @@
+const Joi = require("joi");
+
+const addSchema = Joi.object({
+  name: Joi.string().required().messages({ "any.required": `"name" is a required field` }),
+  email: Joi.string().email().required().messages({ "any.required": `"email" is a required field` }),
+  phone: Joi.string().required().messages({ "any.required": `"phone" is a required field` }),
+});
+
+module.exports = {
+  addSchema,
+};
